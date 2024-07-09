38c49204c1d536819755d94a4ce0b78d37051aec
Jason Xu <dreamergz@gmail.com>
2024-07-09 09:36:30 +0000

    refactor codes for Resume page

---
+++ src/pages/resume/PdfViewer.jsx
@@ -0,0 +1,58 @@
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import { Suspense, useEffect, useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+export default function PdfViewer({ loadWorker, pdf }) {
+    const [numOfPages, setNumOfPages] = useState(0);
+    const [loadedPage, setLoadedPage] = useState(0);
+    const [worker, setWorker] = useState(null);
+    const [pdfData, setPdfData] = useState(null);
+
+    const onDocumentLoadSuccess = (file) => {
+        setNumOfPages(file.numPages);
+    };
+
+    const onPageLoadSuccess = (page) => {
+        setLoadedPage(page.pageNumber);
+    };
+
+    const pages = Array(Math.min(loadedPage + 1, numOfPages))
+        .fill()
+        .map((_item, index) => (
+            <Page
+                className="page"
+                key={index}
+                pageNumber={index + 1}
+                onLoadSuccess={onPageLoadSuccess}
+            />
+        ));
+
+    useEffect(() => {
+        (async () => {
+            setWorker(await loadWorker);
+        })();
+
+        (async () => {
+            const response = await fetch(pdf);
+            const resumeData = new Uint8Array(await response.arrayBuffer());
+            setPdfData({ data: resumeData });
+        })();
+    }, [loadWorker, pdf]);
+    return (
+        <>
+            {!worker && <p>Loading Resume...</p>}
+
+            {worker && pdfData && (
+                <Document
+                    file={pdfData}
+                    onLoadSuccess={onDocumentLoadSuccess}
+                    worker={worker}
+                >
+                    <Suspense>{pages}</Suspense>
+                </Document>
+            )}
+        </>
+    );
+}
--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,48 +1,11 @@
 import "./Resume.css";
-import { Suspense, useEffect, useState, lazy } from "react";
-import "react-pdf/dist/Page/TextLayer.css";
-import "react-pdf/dist/Page/AnnotationLayer.css";
+import { lazy, Suspense } from "react";
 import Button from "../../conponents/button/Button";
 import pdf from "./xu-zhusheng.pdf";
-const loadDocument = import("react-pdf").then(module => ({ default: module.Document}))
-const Document = lazy(() => loadDocument);
-const loadPage = import("react-pdf").then(module => ({ default: module.Page }))
-const Page = lazy(() => loadPage);
+
+const PdfViewer = lazy(() => import("./PdfViewer"));
 
 export default function Resume() {
-    const [numOfPages, setNumOfPages] = useState(0);
-    const [loadedPage, setLoadedPage] = useState(0);
-    const [worker, setWorker] = useState(null)
-    const [pdfData, setPdfData] = useState(null);
-
-    const onDocumentLoadSuccess = (pdf) => {
-        setNumOfPages(pdf.numPages);
-    };
-
-    const onPageLoadSuccess = (page) => {
-        setLoadedPage(page.pageNumber);
-    };
-
-    const pages = Array(Math.min(loadedPage + 1, numOfPages))
-        .fill()
-        .map((_item, index) => (
-            <Page
-                className="page"
-                key={index}
-                pageNumber={index + 1}
-                onLoadSuccess={onPageLoadSuccess}
-            />
-        ));
-    useEffect(() => {
-        import("pdfjs-dist/build/pdf.worker.min.mjs").then(module => setWorker(module));
-        (async () => {
-            const response = await fetch(pdf);
-            const resumeData = new Uint8Array(await response.arrayBuffer());
-            setPdfData({ data: resumeData });
-        })();
-    }, []);
-
-
     return (
         <div id="resume">
             <Button
@@ -50,17 +13,13 @@ export default function Resume() {
                 href={pdf}
                 download="Resume-xuzhusheng.pdf"
             />
-            <Suspense>
-                {worker && (
-                    <Document
-                        file={pdfData}
-                        onLoadSuccess={onDocumentLoadSuccess}
-                        worker={worker}
-                    >
-                        <Suspense>{pages}</Suspense>
-                    </Document>
-                )}
+            <Suspense fallback={<p>Loading Resume...</p>}>
+                <PdfViewer
+                    loadWorker={import("pdfjs-dist/build/pdf.worker.min.mjs")}
+                    pdf={pdf}
+                />
             </Suspense>
+
             <Button
                 text="Download Resume"
                 href={pdf}
