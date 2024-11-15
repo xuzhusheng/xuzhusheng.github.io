72448aedc3a3fa18567f4e72888c45e3973a2e1a
Jason Xu <dreamergz@gmail.com>
2024-11-15 02:59:54 +0000

    remove work experience block

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -45,7 +45,7 @@ export default function Home() {
             <Suspense>
                 <Skills />
                 <Projects />
-                <Experience />
+                {/* <Experience /> */}
             </Suspense>
         </>
     );
